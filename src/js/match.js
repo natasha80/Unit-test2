638582a9bafb6e8a8a83be4d30b3c528fc202722
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-07-27 20:36:53 +0700

    eslint fix

--- src/js/match.js
+++ src/js/match.js
@@ -1,8 +1,8 @@
 export const arrayHeroes = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
 
 export default function showHeroes(data) {
   return data.sort((a, b) => b.health - a.health);
